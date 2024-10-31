5ef662cdb552bc5c0dcc7e41e649a71ac21e92e4
Mykyta1003 <kn23-m.trynko@nubip.edu.ua>
2024-10-31 20:17:23 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,16 +1,23 @@
-"Ваш код повинен зробити POST-запит до вказаного URL."
-"Для створення нового користувача, передайте в запит наступні дані:"
-"name: ваше ім’я"
-"email: ваш email"
-"Поверніть відповідь від сервера після створення користувача."
+const fetch = require('node-fetch');
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
-
-function createUser(user) {
-  // Ваш код
+async function createUser(user) {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    });
+    
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return null;
+  }
 }
 
-console.log(createUser({name: "Sam", email: "fjsnfkjns2342@gmail.com"}))
+createUser({ name: "Sam", email: "fjsnfkjns2342@gmail.com" }).then(result => console.log(result));
 
 module.exports = createUser;
